3c831e5480ad3d983bd3302c62236578e196791f
judiciousmurich <murichjudicious45@gmail.com>
2023-07-06 09:46:09 +0300

    redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ContextProvider } from './context/userContext/Context.jsx'
-import { UIContextProvider } from './context/todoContext/Context.jsx'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import {store,persistor} from './redux/store'
@@ -11,11 +9,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    {/* <ContextProvider> */}
+
       <UIContextProvider >
         <App />
       </UIContextProvider>
-    {/* </ContextProvider> */}
+  
     </PersistGate>
     </Provider>
   </React.StrictMode>,
